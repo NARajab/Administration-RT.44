f3638cd91ca1a1c0e3cceaf1195fcaeecf9e401a
NARajab <11211067@student.itk.ac.id>
2024-07-09 08:47:44 +0800

    modified:   config/database.js 	modified:   vercel.json

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const Sequelize = require("sequelize");
 
 const {
-  DB_USERNAME = "root",
+  DB_USERNAME = "",
   DB_PASSWORD = "",
-  DB_NAME = "kkn-rt44",
-  DB_HOST = "localhost",
-  DB_PORT = "3306", // default MySQL port
+  DB_NAME = "",
+  // DB_HOST = "localhost",
+  // DB_PORT = "3306",
 } = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: "mysql", // use 'mysql' for MySQL
+  // host: DB_HOST,
+  // port: DB_PORT,
+  dialect: "mysql",
 });
 
 const databaseValidation = async () => {
@@ -29,24 +29,24 @@ module.exports = {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
+    // host: DB_HOST,
+    // port: DB_PORT,
     dialect: "mysql", // use 'mysql' for MySQL
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: `${DB_NAME}_test`,
-    host: DB_HOST,
-    port: DB_PORT,
+    // host: DB_HOST,
+    // port: DB_PORT,
     dialect: "mysql", // use 'mysql' for MySQL
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
+    // host: DB_HOST,
+    // port: DB_PORT,
     dialect: "mysql", // use 'mysql' for MySQL
   },
 };
